e0df9f82a2b554255d56ad118d636d14c1fe9c03
Nirajandh <151948730+Nirajandh@users.noreply.github.com>
2024-01-31 18:08:52 +0000

    commit datatypes

---
+++ basics/database.js
@@ -0,0 +1,9 @@
+"use strict " // treat all js code as newer version 
+
+ // alert(3+6) it will not give popup, because we are using node js , not browser
+
+/*console.log(3+3) console.log("niranjan")  ... not allowed because this type of practice neds compalsory semicolen after log
+not a good codinng paractice */
+
+let age='True';
+console.log(typeof NaN);
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let mail="niranjan@gmail.com"
+var pass="12345"
+accountCity="jaipur"
+
+//accountId=123   .... can't modify connst
+mail="xyz@gmail.com"     // can reassign let 
+
+pass="123"
+/*   this is the multiline comment tag
+never reccomended to use var
+*/
+
+console.table([accountId,mail,pass,accountCity])
