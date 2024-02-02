81b28db87c500f6ffb47e5e7e3bc40f5399deb76
Nirajandh <151948730+Nirajandh@users.noreply.github.com>
2024-02-02 16:49:30 +0000

    BUG

--- iterations/loops.js
+++ iterations/loops.js
@@ -63,7 +63,7 @@ console.log(newNums);
 
 //==========================================================================
 
-const myNums = [1, 2, 3]
+const myNum = [1, 2, 3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
